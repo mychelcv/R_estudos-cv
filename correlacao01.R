fbf9edca37a2e0610044d844e7be9b9ff22e7cca
mychelcv <48657998+mychelcv@users.noreply.github.com>
2019-06-08 17:22:16 -0300

    Update correlacao01.R

--- correlacao01.R
+++ correlacao01.R
@@ -1,6 +1,6 @@
 ######## correleção simples 
-x<-c(1,2,3,4,5) # criar um vetor qualquer
-y<-c(6,7,8,9,10) #criar outro vetor
+x<-c(1,2,3,4,5) # cria um vetor qualquer
+y<-c(6,7,8,9,10) #cria outro vetor
 plot(x,y) # faz um gŕafico simples de 'x' com 'y'
 z = data.frame(x,y) 
 z
